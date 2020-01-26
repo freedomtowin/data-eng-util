0920b5a6785650f3200e10a9a41605ab04bf540a
Rohan Kotwani <rohankotwani@icloud.com>
2020-01-26 01:00:50 -0500

    Create data-inspect.py

---
+++ data-inspect.py
@@ -0,0 +1,28 @@
+import os
+
+
+def read_first_line(fname):
+    """
+    Read fist line of file
+    :param filename:
+    :return:
+    """
+    data =[]
+    with open(filename, "r") as data_file:
+        line = data_file.readline()
+        data = line.strip().split()
+    return data  
+    
+
+def main(fname):
+  statinfo = os.stat(fname)
+  size = int(statinfo.st_size)
+  
+  #assert less than 100 gigabytes
+  assert size<1e9
+  
+  column_num = len(read_first_line(fname))
+  
+  
+  
+  
