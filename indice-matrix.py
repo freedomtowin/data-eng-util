46fa28b5816bc5e8bf452482d2297b0a90094ac9
Rohan Kotwani <rohankotwani@icloud.com>
2020-02-11 00:24:44 -0500

    Create indice-matrix.py

---
+++ indice-matrix.py
@@ -0,0 +1,22 @@
+coords = np.vstack([test[['FirstId','SecondId']].values,test[['SecondId','FirstId']].values])
+
+coords = pd.DataFrame(coords,columns = ('FirstId','SecondId'))
+coords = coords.drop_duplicates()
+
+maxId = np.maximum(coords.FirstId.max(),coords.SecondId.max())
+
+data = np.ones(coords.shape[0])
+
+import scipy
+
+scoo = scipy.sparse.coo_matrix((data, (coords.FirstId.values.ravel(), coords.SecondId.values.ravel()))
+                        ,(maxId+1, maxId+1), dtype=np.int8 ) 
+                        
+
+rows_FirstId   = inc_mat[test.FirstId.values]
+rows_SecondId  = inc_mat[test.SecondId.values]
+
+f = rows_FirstId.multiply(rows_SecondId).sum(axis=1)
+
+val,cnts = np.unique(f.A,return_counts=True)
+
